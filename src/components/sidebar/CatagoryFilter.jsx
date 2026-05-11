c1c4f985fe040323eb0be37d255ddeda2f8aa0bc
Sudipta Ghoshal <ghoshalsudipta36@gmail.com>
2026-05-11 22:20:55 +0530

    dynamic catagory added

--- src/components/sidebar/CatagoryFilter.jsx
+++ src/components/sidebar/CatagoryFilter.jsx
@@ -1,31 +1,31 @@
+import { useCatagory } from "../../hooks";
+
 export default function CatagoryFilter() {
-  return (
-    <div className="mb-6">
-      <h4 className="font-medium text-sm mb-3 text-slate-700">Category</h4>
-      <div className="space-y-2">
-        <label className="flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            defaultChecked
-            className="w-4 h-4 text-rose-500 rounded border-slate-300"
-          />
-          <span className="ml-3 text-sm text-slate-700">Apple Mac Pro</span>
-        </label>
-        <label className="flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            className="w-4 h-4 text-rose-500 rounded border-slate-300"
-          />
-          <span className="ml-3 text-sm text-slate-700">Gaming Laptop</span>
-        </label>
-        <label className="flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            className="w-4 h-4 text-rose-500 rounded border-slate-300"
-          />
-          <span className="ml-3 text-sm text-slate-700">Workstation</span>
-        </label>
+  const { catagory, loading, error } = useCatagory();
+
+  if (loading) {
+    return <p className="text-accent">Loading categories...</p>;
+  } else if (error) {
+    return <p className="text-rose-500">Error loading categories: {error}</p>;
+  } else if (catagory.length === 0) {
+    return <p className="text-slate-500">No categories found.</p>;
+  } else {
+    return (
+      <div className="mb-6">
+        <h4 className="font-medium text-sm mb-3 text-slate-700">Category</h4>
+        <div className="space-y-2">
+          {catagory.map((item) => (
+            <label className="flex items-center cursor-pointer" key={item.id}>
+              <input
+                type="checkbox"
+                defaultChecked
+                className="w-4 h-4 text-rose-500 rounded border-slate-300"
+              />
+              <span className="ml-3 text-sm text-slate-700">{item.name}</span>
+            </label>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
